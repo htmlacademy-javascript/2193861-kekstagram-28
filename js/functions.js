42d589a42fb8c713f48d514d8d30492c8a741a3c
deniskirshenin <deniskirshenin@gmail.com>
2023-02-19 21:30:36 +0300

    делаю стрелочные функции

--- js/functions.js
+++ js/functions.js
@@ -1,46 +1,37 @@
 // Функция для проверки длины строки
-function checkStringLength (string, length) {
-  if (string.length <= length) {
-    return true;
-  }
+const checkStringLength = (string, length) => string.length <= length;
 
-  return false;
-}
 
 checkStringLength('htmlacademy', 18);
 
 // Функция для проверки является ли строка палиндромом
 
-function checkPalindrome (string) {
+const checkPalindrome = (string) => {
   let check = '';
 
   for (let i = string.length - 1; i >= 0; i--) {
     check += string[i];
   }
 
-  if (check.trim().toLowerCase() === string.trim().toLowerCase()) {
-    return true;
-  }
-
-  return false;
-}
+  return check.trim().toLowerCase() === string.trim().toLowerCase();
+};
 
 checkPalindrome('топот');
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
 
-function findInteger (string) {
+const findInteger = (string) => {
   if(string.match(/\d+/)) {
     return string.replace(/\D/g, '');
   } else {
     return NaN;
   }
-}
+};
 
 findInteger('ECMAScript 2022');
 
 //  Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины.
-function createFilePath (initial, minLength, additional) {
+const createFilePath = (initial, minLength, additional) => {
   let repeatTimes = (minLength - initial.length) / additional.length;
   if (initial.length < minLength && additional.length < minLength) {
     while (repeatTimes > 0) {
@@ -61,7 +52,7 @@ function createFilePath (initial, minLength, additional) {
   if (initial.length > minLength) {
     return initial;
   }
-}
+};
 
 createFilePath('1', 2, '0');
 createFilePath('1', 4, '0');

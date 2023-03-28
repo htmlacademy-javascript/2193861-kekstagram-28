3814e7399424a3b2491c840bc1fb5c65037487b1
deniskirshenin <deniskirshenin@gmail.com>
2023-03-28 18:17:28 +0300

    вносит правки для соответствия тз

--- js/post-upload.js
+++ js/post-upload.js
@@ -9,6 +9,7 @@ const fileUploadControlElement = document.querySelector('#upload-file');
 const imageUploadOverlayElement = document.querySelector('.img-upload__overlay');
 const fileUploadCloseButtonElement = document.querySelector('#upload-cancel');
 const inputsElements = document.querySelectorAll('input');
+const textDescriptionElement = document.querySelector('.text__description');
 
 fileUploadControlElement.addEventListener('change', () => {
   const file = fileUploadControlElement.files[0];
@@ -21,11 +22,6 @@ fileUploadControlElement.addEventListener('change', () => {
   }
 });
 
-const onFileUploadOpen = () => {
-  imageUploadOverlayElement.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-};
-
 const onFileUploadClose = () => {
   imageUploadOverlayElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
@@ -34,15 +30,13 @@ const onFileUploadClose = () => {
   filterReset();
 };
 
+const onFileUploadOpen = () => {
+  imageUploadOverlayElement.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+};
+
 fileUploadControlElement.addEventListener('change', onFileUploadOpen);
 fileUploadCloseButtonElement.addEventListener('click', onFileUploadClose);
-
-document.addEventListener('keydown', (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    onFileUploadClose();
-  }
-});
 
 inputsElements.forEach((input) => input.addEventListener('keydown', (evt) => {
   if (isEscapeKey(evt)) {
@@ -51,4 +45,11 @@ inputsElements.forEach((input) => input.addEventListener('keydown', (evt) => {
   }
 }));
 
-export {onFileUploadClose};
+textDescriptionElement.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    evt.stopPropagation();
+  }
+});
+
+export {onFileUploadClose, textDescriptionElement};
